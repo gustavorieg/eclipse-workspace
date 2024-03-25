1eeaa2964af9d658110222ca18415c263617fc7e
gustavorieg <gustavorieg10@gmail.com>
2024-03-25 17:09:48 -0300

    Wrapper

--- WrappersProvaA1/src/WrapperIpToBinari.java
+++ WrappersProvaA1/src/WrapperIpToBinari.java
@@ -3,14 +3,15 @@ import java.util.StringTokenizer;
 public class WrapperIpToBinari {
 	public static void main(String[] args) {
 		String classeA = "10.0.0.1";
-		String ClasseB = "138.33.4.8";
-		String ClasseC = "192.168.0.2";
+		String classeB = "138.33.4.8";
+		String classeC = "192.168.0.2";
 		
 		StringTokenizer tokenizer = new StringTokenizer(classeA, ".");
 		
 		while (tokenizer.hasMoreTokens()) {
-            System.out.println(tokenizer.nextToken());
-            
+            String i = tokenizer.nextToken();
+            int a = Integer.parseInt(i);
+            System.out.println(Integer.toBinaryString(a));
         }
 	}
 
